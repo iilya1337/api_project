1f809d1f3f21bd54768468c8e938db4e457dc135
Илюша <whitehood@MacBook-Air-Ilusa.local>
2025-03-17 16:52:08 +0300

    add jwt auth

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
@@ -13,22 +14,15 @@ class LoginController
 {
 
     public function __construct(
-        private readonly SerializerInterface $serializer
-    )
+        private readonly SerializerInterface $serializer,
+    ) { }
+
+    #[Route('/login', name: 'app_login', methods: ['POST'], format: 'json')]
+    public function login(Request $request): void
     {
+        dd($request->toArray());
     }
 
-    #[Route('login', name: 'app_login', methods: ['POST'], format: 'json')]
-    public function login(#[CurrentUser] ?User $user): JsonResponse
-    {
-        if (null === $user) {
-            return $this->json([
-                'message' => 'missing credentials',
-            ], Response::HTTP_UNAUTHORIZED);
-        }
 
-        return $this->json([
 
-        ]);
-    }
 }
